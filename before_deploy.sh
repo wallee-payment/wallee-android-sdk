343639f21722ca7a830593e20834ce757e9f0206
Slobodan Adamovic <slobodan.adamovic@customweb.com>
2017-08-15 11:49:02 +0200

    Add debug message to before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -7,7 +7,11 @@ elif [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
   echo "This is pull request. Skipping decryption of 'codesigning.asc.enc'."
 else
-	echo "[INFO] Decrypting and importing 'codesigning.asc.enc'..."
+  echo "[INFO] Current directory: "
+  pwd
+  echo "[INFO] The listing of current directory: "
+  ls -la; 
+  echo "[INFO] Decrypting and importing 'codesigning.asc.enc'..."
   openssl aes-256-cbc -K $encrypted_da13544aea09_key -iv $encrypted_da13544aea09_iv -in codesigning.asc.enc -out codesigning.asc -d && \
-	gpg --fast-import codesigning.asc;
+  gpg --fast-import codesigning.asc;
 fi
--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ then
   echo "This is pull request. Skipping deployment to maven."
 else
   echo "[INFO] Publishing artifacts to maven."
-  ./gradlew sdk:uploadArchives -PnexusUsername="${OSSRH_JIRA_USERNAME}" -PnexusPassword="${OSSRH_JIRA_PASSWORD}" -Psigning.keyId="${GPG_KEY_ID}" -Psigning.password="${GPG_PASSPHRASE}" -Psigning.secretKeyRingFile="${HOME}/wallee-payment/wallee-android-sdk/codesigning.asc" && \
+  ./gradlew sdk:uploadArchives -PnexusUsername="${OSSRH_JIRA_USERNAME}" -PnexusPassword="${OSSRH_JIRA_PASSWORD}" -Psigning.keyId="${GPG_KEY_ID}" -Psigning.password="${GPG_PASSPHRASE}" -Psigning.secretKeyRingFile="${HOME}/${TRAVIS_REPO_SLUG}/codesigning.asc" && \
   ./gradlew closeAndReleaseRepository
 fi
