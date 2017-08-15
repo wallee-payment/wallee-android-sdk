b3f147f6546964616a9b22d3778ef4c2045aa249
Slobodan Adamovic <slobodan.adamovic@customweb.com>
2017-08-15 10:49:39 +0200

    Implement deployment to maven central from gradle.

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 
 if [ -z "${TRAVIS_TAG}" ];
 then
-	echo "[INFO] This is not tagged build. Skipping deployment to maven.";
+  echo "[INFO] This is not tagged build. Skipping deployment to maven.";
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
   echo "This is pull request. Skipping deployment to maven."
 else
-	echo "[INFO] Publishing artifacts to maven."
-  ./gradlew uploadArchives -PnexusUsername="${OSSRH_JIRA_USERNAME}" -PnexusPassword="${OSSRH_JIRA_PASSWORD}" -Psigning.keyId="${GPG_KEY_ID}" -Psigning.password="${GPG_PASSPHRASE}" -Psigning.secretKeyRingFile=codesigning.asc
+  echo "[INFO] Publishing artifacts to maven."
+  ./gradlew sdk:uploadArchives -PnexusUsername="${OSSRH_JIRA_USERNAME}" -PnexusPassword="${OSSRH_JIRA_PASSWORD}" -Psigning.keyId="${GPG_KEY_ID}" -Psigning.password="${GPG_PASSPHRASE}" -Psigning.secretKeyRingFile=codesigning.asc && \
+  ./gradlew closeAndReleaseRepository
 fi
