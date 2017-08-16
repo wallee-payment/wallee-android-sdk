77bef94c10e74967dcf14c91fb2f2a3037d8254d
Slobodan Adamovic <slobodan.adamovic@customweb.com>
2017-08-16 09:20:42 +0200

    Add new code signing key.

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,17 +2,17 @@
 
 if [ -z "${TRAVIS_TAG}" ];
 then
-  echo "[INFO] This is not tagged build. Skipping decryption of 'codesigning.asc.enc'.";
+  echo "[INFO] This is not tagged build. Skipping decryption of 'signingkey.gpg.enc'.";
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
-  echo "This is pull request. Skipping decryption of 'codesigning.asc.enc'."
+  echo "This is pull request. Skipping decryption of 'signingkey.gpg.enc'."
 else
   echo "[INFO] Current directory: "
   pwd
   echo "[INFO] The listing of current directory: "
   ls -la; 
-  echo "[INFO] Decrypting and importing 'codesigning.asc.enc'..."
-  openssl aes-256-cbc -K $encrypted_da13544aea09_key -iv $encrypted_da13544aea09_iv -in codesigning.asc.enc -out codesigning.asc -d && \
-  gpg --fast-import codesigning.asc && \
+  echo "[INFO] Decrypting and importing 'signingkey.gpg.enc'..."
+  openssl aes-256-cbc -K $encrypted_da13544aea09_key -iv $encrypted_da13544aea09_iv -in signingkey.gpg.enc -out signingkey.gpg -d && \
+  gpg --fast-import signingkey.gpg && \
   gpg --export-secret-keys >${HOME}/.gnupg/secring.gpg;
 fi
